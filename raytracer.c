a0fd190bad3ef20f8857b60de9851c2791116c54
alexandreand <alexandre.alvesandrade@hotmail.com>
2022-04-26 20:37:14 -0300

    first

---
+++ raytracer.c
@@ -0,0 +1,31 @@
+
+using namespace std;
+
+struct runningOptions
+{
+	
+};
+
+int int main()
+{
+	
+
+
+	return 0;
+}
+
+
+int render(){
+	auto h = camera.film.width();
+	auto h = camera.film.height();
+
+	for (int j = 0; j < h; ++j)
+	{
+		for (int  i = 0; i < w; ++i)
+		{
+			auto color = background.sample(float(i)/float(w), float(j)/float(h));
+			camera.film.add(Point2{i, j}, color);
+		}
+	}
+	camera.film.write_image();
+}
