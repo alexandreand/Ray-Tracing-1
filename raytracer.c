4c254629f6bbdcb82c5a7976d93e89a5bcf58e12
alexandreand <alexandre.alvesandrade@hotmail.com>
2022-04-26 21:52:39 -0300

    main

--- raytracer.c
+++ raytracer.c
@@ -9,23 +9,23 @@ struct runningOptions
 int int main()
 {
 	
+	int nx = 200;
+	int ny = 100;
 
+	cout << "P3\n" << nx << " " << ny << "\n255\n";
+	for(int j = ny-1; j >= 0; j--){
+		for (int i = 0; i < nx; i++){
+			float r = float(i) / float(nx);
+			float g = float(j) / float(ny);
+			float b = 0.2;
+			int ir = int(255.99*r);
+			int ig = int(255.99*g);
+			int ib = int(255.99*b);
+			cout << ir << " " << ig << " " << ib << "\n";
+		}
+	}
 
 	return 0;
 }
 
 
-int render(){
-	auto h = camera.film.width();
-	auto h = camera.film.height();
-
-	for (int j = 0; j < h; ++j)
-	{
-		for (int  i = 0; i < w; ++i)
-		{
-			auto color = background.sample(float(i)/float(w), float(j)/float(h));
-			camera.film.add(Point2{i, j}, color);
-		}
-	}
-	camera.film.write_image();
-}
