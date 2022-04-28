5a6fc335abc191d2a148d28b493fc01bbc62759d
alexandreand <chico@pop-os.localdomain>
2022-04-27 21:33:20 -0300

    sei lá

--- include/api.h
+++ include/api.h
@@ -1,2 +1,12 @@
 
-int render();
+class Background {
+    public:
+        Pixel sample(float i, float j){
+            
+        }
+
+    private:
+        Pixel interpolacao(Pixel color_00, Pixel color_01, Pixel color_10, Pixel color_11);
+        Pixel interpolacaoBilinear(Pixel color_1, Pixel color_2)
+
+}
